2b32c6424d42eb61bcd024497ebdc726af76f004
pon1010 <91819186+pon1010@users.noreply.github.com>
2022-03-17 13:20:56 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,9 +1,16 @@
-function header(){
-    $.ajax({
-        url: "pon1010.github.io/header.html",
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
-}
+const loading = document.getElementById('loading');
+
+imagesLoaded( '.l-main', { background: ".demo-kv" }, function() {
+  const msM = 1000;
+  loading.style.transition = 'opacity ' + msM + 'ms';
+  
+  const loadingOpacity = function(){ //透過させる関数を定義
+     loading.style.opacity = 0;
+  }
+  const loadingDisplay = function(){ //非表示にする関数を定義
+    loading.style.display = "none";
+  }
+ 
+  setTimeout(loadingOpacity, 1000); //画像読み込み後、１秒後に透過を開始
+  setTimeout(loadingDisplay, 1000 + msM); //画像読み込み後、２秒後に非表示
+});
