d3a4191262beb7e344de295936bcbe82621cfd8f
pon1010 <91819186+pon1010@users.noreply.github.com>
2022-05-23 16:46:11 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -5,18 +5,21 @@ window.onload = function() {
   spinner.classList.add('loaded');
 }
 
-const pics_src = ["top.jpg","top2.jpg","top3.jpg","top4.jpg"];
-let num = -1;
- 
- 
-function slideshow_timer(){
-  if (num === 2){
-    num = 0;
-  } 
-  else {
-    num ++;
-  }
-  document.getElementById("mypic").src = pics_src[num];
+window.addEventListener('load', function () {
+	viewSlide('.slide img');
+});
+function viewSlide(className, slideNo = -1)
+{
+	let imgArray = document.querySelectorAll(className);
+	if (slideNo >= 0) {
+		//初回以外は現在のスライドを消す
+		imgArray[slideNo].style.opacity = 0;
+	}
+	slideNo++;
+	if (slideNo >= imgArray.length) {
+		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
+	}
+	imgArray[slideNo].style.opacity = 1;
+	let msec = document.getElementById('slide_speed').value;
+	setTimeout(function(){viewSlide(className, slideNo);}, msec);
 }
- 
-setInterval(slideshow_timer, 4000);
