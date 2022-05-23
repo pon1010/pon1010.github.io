63736297fd69e5f5028a09ba9c1a0d3c3d346d1d
pon1010 <91819186+pon1010@users.noreply.github.com>
2022-05-23 18:56:46 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -5,21 +5,16 @@ window.onload = function() {
   spinner.classList.add('loaded');
 }
 
-window.addEventListener('load', function () {
-	viewSlide('.slide img');
-});
-function viewSlide(className, slideNo = -1)
-{
-	let imgArray = document.querySelectorAll(className);
-	if (slideNo >= 0) {
-		//初回以外は現在のスライドを消す
-		imgArray[slideNo].style.opacity = 0;
-	}
-	slideNo++;
-	if (slideNo >= imgArray.length) {
-		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
-	}
-	imgArray[slideNo].style.opacity = 1;
-	let msec = document.getElementById('slide_speed').value;
-	setTimeout(function(){viewSlide(className, slideNo);}, msec);
+
+
+const img = ["https://dubdesign.net/wp-content/uploads/2022/01/pagereload.jpg", "https://dubdesign.net/wp-content/uploads/2022/01/javaprogless.jpg", "https://dubdesign.net/wp-content/uploads/2021/12/cookiewrite.jpg"];
+let count = -1;
+picChange();
+function picChange() {
+  count++;
+  if (count == img.length) count = 0;
+  //画像選択
+  changePic.src = img[count];
+  //秒数の指定
+  setTimeout("picChange()", 4000);
 }
