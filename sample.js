dc23cec4f06630daa454f5cdd441dd4720732996
pon1010 <91819186+pon1010@users.noreply.github.com>
2022-05-23 15:24:42 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -4,3 +4,19 @@ window.onload = function() {
   // Add .loaded to .loading
   spinner.classList.add('loaded');
 }
+
+const pics_src = ["pics/1.jpg","pics/2.jpg","pics/3.jpg"];
+let num = -1;
+ 
+ 
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 1000);
