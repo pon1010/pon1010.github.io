8b688fbcda5c32170fc002e9e4d0b332f2ac5fd9
pon1010 <91819186+pon1010@users.noreply.github.com>
2022-05-24 11:33:33 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -7,14 +7,3 @@ window.onload = function() {
 
 
 
-const img = ["https://dubdesign.net/wp-content/uploads/2022/01/pagereload.jpg", "https://dubdesign.net/wp-content/uploads/2022/01/javaprogless.jpg", "https://dubdesign.net/wp-content/uploads/2021/12/cookiewrite.jpg"];
-let count = -1;
-picChange();
-function picChange() {
-  count++;
-  if (count == img.length) count = 0;
-  //画像選択
-  changePic.src = img[count];
-  //秒数の指定
-  setTimeout("picChange()", 4000);
-}
